dc29d3d797b0ed040ac511373db69a5507bf6989
fviel <fernandovie82@gmail.com>
2021-03-04 23:48:53 -0300

    exibição de lista de objetos

---
+++ lib/frase.dart
@@ -0,0 +1,11 @@
+class Frase {
+  String frase;
+  String autor;
+
+  // Frase(String frase, String autor){
+  //   this.frase = frase;
+  //   this.autor = autor;
+  // }
+
+  Frase({this.frase, this.autor});
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'frase.dart';
 
 void main() {
   runApp(MaterialApp(
@@ -14,14 +15,16 @@ class QuoteList extends StatefulWidget {
 
 //Minha classe State
 class _QuoteListState extends State<QuoteList> {
-  List<String> frases = [
-    'Podia ser melhor...',
-    'Eu queria poder comprar meu novo notebook',
-    'Quero terminar o bootcamp do Inter',
-    'Quero fazer meus apps em Flutter',
-    'Ainda espero fazer meu app de perguntas e respostas',
-    'Ainda espero fazer o Responder Questões!'
+  List<Frase> frases = [
+    Frase(frase: 'Podia ser melhor...', autor: 'Fernando'),
+    Frase(frase: 'Eu queria poder comprar meu novo notebook', autor: 'Simone'),
+    Frase(frase: 'Quero terminar o bootcamp do Inter', autor: 'Helena'),
+    Frase(frase: 'Quero fazer meus apps em Flutter', autor: 'Jade'),
+    Frase(frase: 'Ainda espero fazer meu app de perguntas e respostas',autor: 'Monza'),
+    Frase(frase: 'Ainda espero fazer o Responder Questões!', autor: 'Sandero')
   ];
+
+  //List<String> autores = ['Fernando', 'Viel', 'Helena', 'Simone', 'Jade'];
 
   @override
   Widget build(BuildContext context) {
@@ -37,20 +40,21 @@ class _QuoteListState extends State<QuoteList> {
         mainAxisAlignment: MainAxisAlignment.start,
         crossAxisAlignment: CrossAxisAlignment.start,
 
-        children: frases
-            .map(
-              (frase) => Text(
-                frase,
-                style: TextStyle(
-                  //backgroundColor: Colors.blue,
-                  fontSize: 10.0,
-                  fontWeight: FontWeight.bold,
-                  letterSpacing: 2.0,
-                  color: Colors.grey[600],
-                  fontFamily: 'Roboto',
-                ),
-              )
-            ).toList(),
+          children: frases.map((frase)=>Text('${frase.frase} - ${frase.autor}')).toList(),
+
+        // children: frases
+        //     .map((frase) => Text(
+        //           '${frase.frase} - ${frase.autor}',
+        //           style: TextStyle(
+        //             //backgroundColor: Colors.blue,
+        //             fontSize: 10.0,
+        //             fontWeight: FontWeight.bold,
+        //             letterSpacing: 2.0,
+        //             color: Colors.grey[600],
+        //             fontFamily: 'Roboto',
+        //           ),
+        //         ))
+        //     .toList(),
 
         //Este método publicou meu array como uma lista de widgets, cada widget recebeu uma posição do meu array, e há duas formas de fazer isso:
         //Forma A - children: frases.map((frase)=>Text(frase)).toList();
