b1fc5e54bfe3262c1dbda44b0de17e7546c67231
fviel <fernandovie82@gmail.com>
2021-03-04 22:27:32 -0300

    Initial commit

---
+++ android/app/src/main/java/com/fernando/quotes/MainActivity.java
@@ -0,0 +1,6 @@
+package com.fernando.quotes;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: 
+  ));
+}
+
+class  extends StatefulWidget {
+  @override
+  _State createState() => _State();
+}
+
+class _State extends State<> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
+
+
